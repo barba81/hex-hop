79e6b80d3688a1613bc6ab0784887e0e942aef50
sablastBF <filip.barbaric99@gmail.com>
2026-04-20 21:44:51 +0200

    better drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const CloseBar = () => {
   return (
     <>
     <div data-tauri-drag-region className="fixed left-1/2 -translate-x-1/2 h-5 w-40 bg-stone-600 dark:bg-stone-100 rounded-b-2xl cursor-pointer " />
-      <div className="flex  w-full justify-end gap-2 px-1">
+      <div data-tauri-drag-region  className="flex  w-full justify-end gap-2 pt-1 pr-1">
         <div
           className="text-gray-900 dark:text-white cursor-pointer hover:bg-gray-400/50 dark:hover:bg-gray-800/50 p-1 rounded-md"
           onClick={() => {
