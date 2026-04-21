cd3dc5b44137e36e9e8222aa9457357868d76c8a
sablastBF <filip.barbaric99@gmail.com>
2026-04-21 22:06:02 +0200

    barly connected chouser with state.

--- src/components/core/ColorPicker.tsx
+++ src/components/core/ColorPicker.tsx
@@ -4,6 +4,7 @@ import { Input } from "../ui/input";
 import { Check, Pipette } from "lucide-react";
 import { HexColorPicker } from "react-colorful";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { useColorStore } from "@/store/useColorStore";
 
 declare global {
   interface Window {
@@ -14,41 +15,48 @@ declare global {
 }
 const SelectNewColor = () => {
   const [color, setColor] = useState("#3b82f6");
+  const setCurrentColor = useColorStore().setColor;
 
   return (
     <div className="flex items-center gap-3">
       <Popover>
         <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            className="w-10 h-10 p-0 cursor-pointer border-2 overflow-hidden"
+          <div
+            className="
+            rounded-md 
+            w-10 h-10 p-0 
+            cursor-pointer 
+            outline-2 overflow-hidden"
             style={{ backgroundColor: color }}
-          ></Button>
+          ></div>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-3">
-          <HexColorPicker color={color} onChange={setColor} />
+          <HexColorPicker
+            color={color}
+            onChange={(color) => {
+              setColor(color);
+              setCurrentColor(color);
+            }}
+          />
         </PopoverContent>
       </Popover>
     </div>
   );
 };
 const ColorPicker = () => {
-  const [, setSelectedColor] = useState("#ffffff");
+  const currentColor = useColorStore().currentColor;
+  const setColor = useColorStore().setColor;
 
   const handlePickColor = async () => {
-    // 1. Check if the browser supports the EyeDropper API
     if (!window.EyeDropper) {
-      alert("Your browser does not support the EyeDropper API.");
       return;
     }
 
     const eyeDropper = new window.EyeDropper();
 
     try {
-      // 2. Open the pipette tool
       const result = await eyeDropper.open();
-      // 3. The result returns an object: { sRGBHex: '#000000' }
-      setSelectedColor(result.sRGBHex);
+      setColor(result.sRGBHex);
     } catch (e) {
       console.log("Color selection cancelled or failed");
     }
@@ -65,10 +73,11 @@ const ColorPicker = () => {
       <div
         className="
           flex  
+          outline-2
           items-center 
           justify-center
           rounded-md
-          w-12 h-10 p-0 cursor-pointer  overflow-hidden
+          w-18 h-10 p-0 cursor-pointer  overflow-hidden
         bg-foreground/20
         hover:bg-foreground/25
           text-gray-900 dark:text-white "
@@ -80,19 +89,22 @@ const ColorPicker = () => {
       </div>
       <SelectNewColor />
 
-      <div className="flex justify-center items-center gap-1">
-        <Input className="h-10 border-0" placeholder="Enter color" />
-        <div
-          className="
+      <Input
+        className="h-11 border-2"
+        placeholder="Enter color"
+        value={currentColor}
+      />
+      <div
+        className="
           flex  
           items-center 
+          outline-2
           justify-center
           rounded-md
-          w-13 h-10 p-0 cursor-pointer  overflow-hidden bg-green-400/30 hover:bg-green-400/40 text-gray-900 dark:text-white "
-          onClick={() => addColor()}
-        >
-          <Check strokeWidth={3} />
-        </div>
+          w-19 h-10 p-0 cursor-pointer  overflow-hidden bg-green-400/30 hover:bg-green-400/40 text-gray-900 dark:text-white "
+        onClick={() => addColor()}
+      >
+        <Check strokeWidth={2.5} />
       </div>
     </div>
   );
--- src/store/useColorStore.ts
+++ src/store/useColorStore.ts
@@ -1,11 +1,11 @@
 import { create } from 'zustand';
 
 interface ColorState {
-  color: string;
+  currentColor: string;
   setColor: (color: string) => void;
 }
 
 export const useColorStore = create<ColorState>((set) => ({
-  color: "#3b82f6", // Initial color
-  setColor: (newColor) => set({ color: newColor }),
+  currentColor: "#3b82f6", 
+  setColor: (newColor) => set({ currentColor: newColor }),
 }));
