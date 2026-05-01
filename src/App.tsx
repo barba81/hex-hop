dbb9edcfc91d11d40dd646d7afa6b0ed84691d6b
sablastBF <filip.barbaric99@gmail.com>
2026-05-01 16:41:31 +0200

    remove toster

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { moveWindow, Position } from "@tauri-apps/plugin-positioner";
 import { useTheme } from "./hooks/useTheme";
 import { ColorPallet } from "./service/colorPallet";
-import { Toaster } from "sonner";
 import HeaderBar from "./pages/HexHop/header/HeaderBar";
 import ColorList from "./pages/HexHop/colorList/ColorList";
 import ColorPicker from "./pages/HexHop/colorPicker/ColorPicker";
@@ -25,14 +24,7 @@ function HexHopApp() {
   }, []);
   return (
     <>
-      <div className="w-screen h-screen flex flex-col gap-0.5 dark:bg-stone-800/80 bg-stone-200/80">
-      <Toaster 
-          theme="dark" 
-          position="bottom-center"
-          toastOptions={{
-            className: " text-xs py-2 px-3 flex justify-center",
-          }}
-        />
+      <div className="w-screen h-screen flex flex-col gap-0.5 dark:bg-stone-800/80 bg-stone-200/80 overflow-visible">
         <HeaderBar />
         <ColorList />
         <ColorPicker />
