2829738085949a693a35b372b3407b54d6688aad
sablastBF <filip.barbaric99@gmail.com>
2026-06-15 01:05:47 +0200

    adding gradient repo

--- src/features/gradient/add-new-gradient-layer/add-new-gradient-layer.ts
+++ src/features/gradient/add-new-gradient-layer/add-new-gradient-layer.ts
@@ -1,23 +1,32 @@
 import { GradientLayer } from "@/features/infrastructure/entity/gradient.entity";
+import { insertGradientLayerAsync } from "@/repo/gradient/gradient-layer";
 import { useGradientStore } from "@/store/use-gradient-store";
+
 
 export const addNewGradientLayer = (gradientId: number | null) => {
     if (gradientId === null) return;
 
-    // insert into repo
-    
-    
-    // insert into store
+    const state =    useGradientStore.getState();
+    const gradient = state.gradients.find(x => x.id === gradientId);
+    if (!gradient) return;
+    const order =
+    gradient.layers.length;
 
     const gradientLayer: GradientLayer = {
         id: 0,
         colorSpace: 'longer hue',
         easingFunction: 0,
-        order: 0,
+        order,
         gradientType: "linear",
         rotationDegree: 0,
         patternRepeatNumber: 0,
         stops: []
     };
-     useGradientStore.getState().actions.addLayerToSelected(gradientLayer);
+    // insert into repo
+     insertGradientLayerAsync(gradientLayer, gradientId);
+
+    
+    // insert into store
+
+    state.actions.addLayerToSelected(gradientLayer);
 }
---
+++ src/repo/gradient/gradient-layer.ts
@@ -0,0 +1,31 @@
+import { getContext } from "@/features/infrastructure/client";
+import { GradientLayerDto } from "./gradient-dto";
+import { GradientLayer } from "@/features/infrastructure/entity/gradient.entity";
+
+export const insertGradientLayerAsync = async (gradientLayer: GradientLayer , gradientId: number) => {
+    const db = await getContext();
+
+    try {
+        return await db.execute(`
+            INSERT INTO 
+            gradient_layer 
+            (gradientId, gradientType, rotationDegree, patternRepeatNumber, colorSpace, easingFunction ) 
+            VALUES 
+            (?,?,?,?,?,?) `,
+            [gradientId, gradientLayer.gradientType, gradientLayer.rotationDegree, gradientLayer.patternRepeatNumber, gradientLayer.colorSpace, gradientLayer.easingFunction]);
+    } catch (error) {
+        console.error("Transaction aborted and rolled back:", error);
+        throw error;
+    }
+}
+
+export const getAllGradientLayersAsync = async () => {
+    const db = await getContext();
+    
+    try {
+        return await db.select<GradientLayerDto[]>('SELECT * FROM gradient');
+    } catch (error) {
+        console.error("Transaction aborted and rolled back:", error);
+        throw error;
+    }
+}
