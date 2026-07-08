a2e59b46325c8a47ac54fb058bc275b071489ade
sablastBF <filip.barbaric99@gmail.com>
2026-07-08 22:08:30 +0200

    adding new icons and new way to import gradient icons

---
+++ src/components/icons/gradient-step-icon.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+interface IconProps extends React.ComponentPropsWithoutRef<"svg"> {
+  size?: string | number;
+  color?: string;
+  strokeWidth?: string | number;
+}
+
+export const GradientStepIcon = ({
+  size = 24,                 
+  color = "currentColor",
+  strokeWidth = 1.07,         
+  className = '',
+  ...props 
+}: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width={size}            
+      height={size}          
+      viewBox="0 0 99 99"
+      className={className}   
+      {...props}
+    >
+      <defs>
+        <linearGradient id="a">
+          {/* Wire up 'color' to stopColor instead of hardcoded '#000' */}
+          <stop offset={0} style={{ stopColor: color, stopOpacity: 1 }} />
+          <stop offset={0.25} style={{ stopColor: color, stopOpacity: 1 }} />
+          <stop offset={0.25} style={{ stopColor: color, stopOpacity: 0.75 }} />
+          <stop offset={0.5} style={{ stopColor: color, stopOpacity: 0.75 }} />
+          <stop offset={0.5} style={{ stopColor: color, stopOpacity: 0.5 }} />
+          <stop offset={0.75} style={{ stopColor: color, stopOpacity: 0.5 }} />
+          <stop offset={0.75} style={{ stopColor: color, stopOpacity: 0.01176471 }} />
+        </linearGradient>
+        <linearGradient
+          xlinkHref="#a"
+          id="b"
+          x1={11.035}
+          x2={108.965}
+          y1={60}
+          y2={60}
+          gradientUnits="userSpaceOnUse"
+        />
+      </defs>
+      <rect
+        width={97.93}
+        height={97.93}
+        x={11.035}
+        y={11.035}
+        rx={10.756}
+        style={{
+          fill: "url(#b)",
+          stroke: color,       // Wire up color to the border stroke
+          strokeWidth: strokeWidth, // Wire up dynamic stroke weight variable
+          strokeLinejoin: "round",
+          strokeDasharray: "none",
+          strokeOpacity: 1,
+        }}
+        transform="translate(-10.5 -10.5)"
+      />
+    </svg>
+  );
+};
--- src/pages/gradient-generator/gradient-generator-page.tsx
+++ src/pages/gradient-generator/gradient-generator-page.tsx
@@ -2,8 +2,10 @@ import GradientLayerList from "./gradient-layer/gradient-layer-list";
 import GradientPreviewBox from "./gradient-preview";
 import { AllGradientsList } from "./all-gradients-list/all-gradient-list";
 import GradientInfo from "./gradient-info";
-import {useGradientStoreHasElements} from "@/store/use-gradient-store";
+import { useGradientStoreHasElements } from "@/store/use-gradient-store";
 import GradientLayerListHeader from "./gradient-layer/gradient-layer-list-header";
+import { Plus } from "lucide-react";
+import { GradientStepIcon } from "@/components/icons/gradient-step-icon";
 
 const GradientGeneratorPage = () => {
   const hasElements = useGradientStoreHasElements();
@@ -13,20 +15,31 @@ const GradientGeneratorPage = () => {
       <>
         <GradientInfo />
         <GradientPreviewBox gradientBackground=" linear-gradient(4deg,rgba(63, 94, 251, 1) 0%, rgba(152, 83, 183, 1) 31%, rgba(252, 70, 107, 1) 100%)" />
-        <GradientLayerListHeader/>
+        <GradientLayerListHeader />
         <GradientLayerList />
       </>
     );
   };
 
   const EmptyGradientList = () => {
-    return <><div>Add new gradietns</div></>
-  }
+    return (
+      <>
+        <div className="h-full w-full flex flex-col items-center justify-center">
+          <div>
+            <GradientStepIcon size={150}  />
+          </div>
+          <div>ADD NEW GRADIENT</div>
+          <div>
+            <Plus />
+          </div>
+        </div>
+      </>
+    );
+  };
 
   return (
     <>
       <div className="flex flex-col w-full flex-1 p-1 gap-2">
-        <AllGradientsList />
         {hasElements ? <GradientPreview /> : <EmptyGradientList />}
       </div>
     </>
