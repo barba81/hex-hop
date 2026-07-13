1954058b2fcea338cb4de37c1994e4533d42a46e
sablastBF <filip.barbaric99@gmail.com>
2026-07-13 23:47:01 +0200

    this solve the problem.

--- src-tauri/src/repo/gradient.rs
+++ src-tauri/src/repo/gradient.rs
@@ -7,6 +7,10 @@ pub async fn create_gradient(
     tx: &mut  Transaction<'_, Sqlite> 
 ) -> Result<(), sqlx::Error> {
     
+    let id: i64 = sqlx::query_scalar("INSERT INTO gradient (name) VALUES ($1) RETURNING id")
+        .bind(gradient.name)
+        .fetch_one(tx.as_mut()) // <-
+        .await?;
 
     Ok(())
 }
