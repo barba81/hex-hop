f059aec558f9323e4534048b3c55ea5fedf3a3c0
sablastBF <filip.barbaric99@gmail.com>
2026-07-21 22:48:31 +0200

    seting up ui

--- src/pages/gradient-generator/gradient-layer/gradient-layer-list.tsx
+++ src/pages/gradient-generator/gradient-layer/gradient-layer-list.tsx
@@ -1,11 +1,31 @@
 import "@/style/empty-checker-board.css";
-import GradientLayer from "./gradient-layer";
-
-const GradientLayerList = () => {
-
+import { GradientEntity } from "@/features/infrastructure/entity/gradient.entity";
+interface GradientLayerListParm {
+  gradient: GradientEntity;
+}
+const GradientLayerList = ({ gradient }: GradientLayerListParm) => {
   return (
-    <div className="flex-1 overflow-y-auto  flex flex-col gap-3 py-1 bg-stone-800">
-      
+    <div className="flex-1 overflow-y-auto  flex  gap-3 py-1 ">
+      {gradient.layers.map((layer, ix) => (
+        <div key={ix} className="flex w-full flex-col">
+          <div className="flex w-full justify-between">
+            <div>{layer.id}</div>
+            <div>{layer.patternRepeatNumber}</div>
+            <div>{layer.colorSpace}</div>
+            <div>{layer.easingFunction}</div>
+          </div>
+          <div>
+            {layer.stops.map((stop, ix) => (
+              <div className="flex flex-row gap-5">
+                <div key={ix}>{stop.r}</div>
+                <div key={ix}>{stop.g}</div>
+                <div key={ix}>{stop.b}</div>
+                <div key={ix}>{stop.a}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
