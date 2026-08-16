f169f210fac9cef84c9f34bf7ff5e70631f8333c
sablastBF <filip.barbaric99@gmail.com>
2026-08-17 00:34:08 +0200

    add command for update

--- src/pages/color-clipboard/features/delete-block.ts
+++ src/pages/color-clipboard/features/delete-block.ts
@@ -1,14 +1,28 @@
+import { useColorListCommands } from "@/infrastructure/command/command-manager-provider";
+import { ColorEntity } from "@/infrastructure/models/entity";
 import { useClipboardStore } from "@/pages/color-clipboard/store/use-clipboard-store";
 import { invoke } from "@tauri-apps/api/core";
 
-export const deleteBlock = async (blockId: number, paletteId?: number) => {
-    await invoke("soft_delete_block", {blockId:blockId});
+export const deleteBlock = async (blockId: number, paletteId: number | null) => {
+    await invoke("soft_delete_block", { blockId: blockId });
     useClipboardStore.getState().deleteBlock(blockId, paletteId);
-} 
+
+    useColorListCommands.getState().push({
+        async undo() {
+            await invoke("restore_block", { blockId });
+            const entity = await invoke<ColorEntity>("get_color", { colorId:'1' });
+            useClipboardStore.getState().addBlock(entity);
+        },
+        async redo() {
+            await invoke("soft_delete_block", { blockId: blockId });
+            useClipboardStore.getState().deleteBlock(blockId, paletteId);
+        },
+    });
+}
 
 // const deletePaletteBlocks = async (_: number) => {
-    // const blocks = await invoke<BlockEntity[]>("load_state");
-    // useClipboardStore.getState().initBlocks(blocks);
+// const blocks = await invoke<BlockEntity[]>("load_state");
+// useClipboardStore.getState().initBlocks(blocks);
 // }
 
 export const deleteClipboard = async () => {
--- src/pages/color-clipboard/features/update-block.ts
+++ src/pages/color-clipboard/features/update-block.ts
@@ -1,9 +1,26 @@
 import type { ColorEntity } from "@/infrastructure/models/entity"
 import { invoke } from "@tauri-apps/api/core";
 import { useClipboardStore } from "../store/use-clipboard-store";
+import { useColorListCommands } from "@/infrastructure/command/command-manager-provider";
 
 export const updateColorBlock = async (newEntity: ColorEntity) => {
-    await invoke("update_color", { color: { ...newEntity} });
+    const oldEntity = await invoke<ColorEntity>("get_color", { colorId: newEntity.id });
+
+    await invoke("update_color", { color: { ...newEntity } });
     const colorEntity = await invoke<ColorEntity>("get_color", { colorId: newEntity.id });
+
     useClipboardStore.getState().updateBlock(colorEntity);
+
+    useColorListCommands.getState().push({
+        async undo() {
+            await invoke("update_color", { color: { ...oldEntity } });
+            const colorEntity = await invoke<ColorEntity>("get_color", { colorId: newEntity.id });
+            useClipboardStore.getState().updateBlock(colorEntity);
+        },
+        async redo() {
+            await invoke("update_color", { color: { ...newEntity } });
+            const colorEntity = await invoke<ColorEntity>("get_color", { colorId: newEntity.id });
+            useClipboardStore.getState().updateBlock(colorEntity);
+        },
+    });
 }
