26f091529517754da4651753b74b6416720cce4e
Filip <filip.brabaric@outlook.com>
2026-09-21 17:37:28 +0200

    swap drad and rop for icon

--- src/components/custom/drag-and-drop/drag-dots.tsx
+++ src/components/custom/drag-and-drop/drag-dots.tsx
@@ -1,19 +1,7 @@
-const Dot = () => {
-  return <span className="h-0.75 w-0.75 rounded-full bg-current" />;
-};
+import { GripVertical } from "lucide-react";
+
 export const DragDots = () => {
   return (
-    <div className="relative cursor-pointer h-full w-4 gap-0.5 flex items-center justify-center bg-secondary  px-1 ">
-        <div className="w-2  flex flex-col items-center justify-center gap-1 ">
-          <Dot />
-          <Dot />
-          <Dot />
-        </div>
-        <div className=" w-2 flex flex-col items-center justify-center gap-1">
-          <Dot />
-          <Dot />
-          <Dot />
-        </div>
-      </div>
+    <GripVertical size={22}/>
   );
 };
--- src/components/header/mac-header-button.tsx
+++ src/components/header/mac-header-button.tsx
@@ -1,28 +1,29 @@
+import { Button } from "@base-ui/react";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 
 const MacHeaderButton = () => {
   const appWindow = getCurrentWindow();
 
   return (
-    <div className="flex gap-[8px] px-3 items-center group">
-      <button
+    <div className="flex gap-2 px-3 items-center group">
+      <Button
         onClick={() => appWindow.close()}
-        className="w-4 h-4 rounded-full bg-[#ff5f57] border-[0.5px] border-[#e0443e] flex items-center justify-center cursor-default relative"
+        className="cursor-pointer w-4 h-4 rounded-full bg-[#ff5f57] border-[0.5px] border-[#e0443e] flex items-center justify-center"
       >
         <span className="hidden group-hover:block text-[8px] text-[#4c0000] font-bold">✕</span>
-      </button>
+      </Button>
 
-      <button
+      <Button
         onClick={() => appWindow.minimize()}
-        className="w-4 h-4 rounded-full bg-[#febc2e] border-[0.5px] border-[#d8a124] flex items-center justify-center cursor-default"
+        className="cursor-pointer  w-4 h-4 rounded-full bg-[#febc2e] border-[0.5px] border-[#d8a124] flex items-center justify-center"
       >
-        <span className="hidden group-hover:block text-[10px] text-[#5c3c00] font-bold mb-[2px]">−</span>
-      </button>
+        <span className="hidden group-hover:block text-[10px] text-[#5c3c00] font-bold mb-0.5">-</span>
+      </Button>
 
-      <div
+      <Button
         className="w-4 h-4 rounded-full bg-[#e6e6e6] dark:bg-[#3d3d3d] border-[0.5px] border-[#d1d1d1] dark:border-[#2b2b2b] flex items-center justify-center cursor-default"
       >
-      </div>
+      </Button>
     </div>
   );
 };
